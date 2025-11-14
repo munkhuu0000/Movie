cd6f02067f02bbf340e57b71ddb07c8281cea10c
25LP8703 25LP8703 <25LP8703@pinecones-iMac.local>
2025-11-14 12:34:05 +0800

    change1

---
+++ src/app/_components/Header.js
@@ -0,0 +1,4 @@
+"use client";
+export const Header = (props) => {
+  return <div className="w-screen h-screen bg-amber-800"></div>;
+};
---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+"use client";
+import { Header } from "@/app/_components/Header";
+
+export default function Home() {
+  return <Header />;
+}

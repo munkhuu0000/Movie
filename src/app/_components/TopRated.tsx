55cc0a263b99f93640e780fe19eead813ced9eb1
Munkhuu0000 <munhbat4486@gmail.com>
2025-11-23 16:14:30 +0800

    change106

--- src/app/_components/TopRated.tsx
+++ src/app/_components/TopRated.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Movie = {
   adult: Boolean;
@@ -27,8 +28,10 @@ type Response = {
 
 export const TopRated = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    setLoading(true);
     const GetData = async () => {
       const res = await fetch(
         "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -48,37 +51,64 @@ export const TopRated = () => {
     };
 
     GetData();
+    setLoading(false);
   }, []);
 
   return (
-    <div className="w-screen h-[1038px] px-20 gap-15">
-      <div className="w-100% h-9 flex justify-between items-center">
-        <p className="font-semibold text-2xl">Top Rated</p>
-        <div className="flex flex-row gap-3 ">
-          <p className="font-medium text-sm">See more</p>
-          <FaArrowRightLong />
-        </div>
-      </div>
-      <div className="w-100% grid grid-rows-2 grid-cols-5 gap-8 justify-center ">
-        {movies.slice(0, 10).map((movie) => (
-          <div key={movie.id} className="w-full h-[440px] bg-muted rounded-lg">
-            <div className="w-full h-[340px] rounded-t-lg hover:bg-black hover:opacity-50 hover:duration-300 hover:ease-in-out z-2 ">
-              <img
-                className="w-full h-[340px] rounded-t-lg z-10 "
-                src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
-                alt={movie?.title}
+    <div className="w-screen gap-15">
+      {loading ? (
+        <div className="flex flex-col gap-8 px-20">
+          <div className="w-100% h-9 flex justify-between items-center">
+            <Skeleton className="w-[250px] h-8 rounded-full"></Skeleton>
+            <Skeleton className="w-[250px] h-8 rounded-full"></Skeleton>
+          </div>
+          <div className="w-100% grid grid-rows-2 grid-cols-5 gap-8 justify-center ">
+            {[...Array(10)].map((_, i) => (
+              <Skeleton
+                key={i}
+                className="w-full h-[440px] bg-muted rounded-lgl"
               />
-            </div>
-            <div className="w-full h-[100px] px-3">
-              <div className="flex flex-row items-center gap-1">
-                <FaStar className="h-4 w-4 fill-[#FDE047]" />
-                <p>{movie.vote_average.toFixed(1)}/10</p>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-8 ">
+          <div className="w-screen gap-15 px-20">
+            <div className="w-100% h-9 flex justify-between items-center">
+              <p className="font-semibold text-2xl">Top Rated</p>
+              <div className="flex flex-row gap-3 ">
+                <p className="font-medium text-sm">See more</p>
+                <FaArrowRightLong />
               </div>
-              <p className="text-lg font-normal">{movie.title}</p>
+            </div>
+            <div className="w-100% grid grid-rows-2 grid-cols-5 gap-8 justify-center ">
+              {movies.slice(0, 10).map((movie) => (
+                <div
+                  key={movie.id}
+                  className="w-full h-[440px] bg-muted rounded-lg"
+                >
+                  <div className="w-full h-[340px] rounded-t-lg hover:bg-black hover:opacity-50 hover:duration-300 hover:ease-in-out z-2 ">
+                    <img
+                      className="w-full h-[340px] rounded-t-lg z-10 "
+                      src={
+                        "https://image.tmdb.org/t/p/w500/" + movie.poster_path
+                      }
+                      alt={movie?.title}
+                    />
+                  </div>
+                  <div className="w-full h-[100px] px-3">
+                    <div className="flex flex-row items-center gap-1">
+                      <FaStar className="h-4 w-4 fill-[#FDE047]" />
+                      <p>{movie.vote_average.toFixed(1)}/10</p>
+                    </div>
+                    <p className="text-lg font-normal">{movie.title}</p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/app/_components/UpComing.tsx
+++ src/app/_components/UpComing.tsx
@@ -108,25 +108,7 @@ export const UpComing = () => {
           </div>
         </div>
       )}
-      {/* <div className="w-100% grid grid-rows-2 grid-cols-5 gap-8 justify-center ">
-        {movies.slice(0, 10).map((movie) => (
-          <div key={movie.id} className="w-full h-[440px] bg-muted rounded-lg">
-            <div className="w-full h-[340px] rounded-t-lg hover:bg-black hover:opacity-50 hover:duration-300 hover:ease-in-out z-2 ">
-              <img
-                className="w-full h-[340px] rounded-t-lg z-10 "
-                src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
-              />
-            </div>
-            <div className="w-full h-[100px] px-3">
-              <div className="flex flex-row items-center gap-1">
-                <FaStar className="h-4 w-4 fill-[#FDE047]" />
-                <p>{movie.vote_average.toFixed(1)}/10</p>
-              </div>
-              <p className="text-lg font-normal">{movie.title}</p>
-            </div>
-          </div>
-        ))}
-      </div> */}
+      ;
     </div>
   );
 };
